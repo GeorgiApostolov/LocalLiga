565f3b3817820cf5bade01523ba44c04bc9622db
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2025-11-10 19:36:30 +0200

    add Header component with simple inline style

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header style={{ background: "#11", color: "#fff", padding: "1rem" }}>
+      <h2>LocalLiga ⚽</h2>
+    </header>
+  );
+}
+
+export default Header;
