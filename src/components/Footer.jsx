7f166bea48466bcb9adc3b79b1dbef10925279ad
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2025-11-10 19:38:44 +0200

    add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer
+      style={{ textAlign: "center", padding: "1rem", fontSize: "0.9rem" }}
+    >
+      <p>© 2025 LocalLiga - All rights reserved.</p>
+    </footer>
+  );
+}
+export default Footer;
