de329ef7a4a33b7e7386efafa7cd298c157213c6
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2025-12-04 12:27:43 +0200

    wrap App with AuthProvider in main entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/global.css";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
